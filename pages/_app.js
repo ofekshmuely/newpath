a522dc7d919d26f0440e6ba626106b26154a7f53
Ofek Shmuely <meshuba.global@gmail.com>
2023-09-05 16:22:56 +0300

    update mainframe

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { Analytics } from "@varcel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
