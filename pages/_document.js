2ffd3c5f29aef2b290e366b9d526de11bc13d065
Ofek Shmuely <meshuba.global@gmail.com>
2023-09-05 16:06:07 +0300

    update mainframe

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Script } from "next/script";
 
 export default function Document() {
   return (
@@ -8,7 +9,17 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
-        <script src="../pages/analyisis.js"></script>
+        <NextScript
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7CKE5FMHP4`}
+        />
+        <NextScript id="ga-script" strategy="lazyOnload">
+          {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-7CKE5FMHP4',
+         {page_path: window.location.pathname,}); `}
+        </NextScript>
       </Head>
       <body>
         <Main />
