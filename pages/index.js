fb8db08422b587e5945caccd241dc02a8d5fc789
Ofek Shmuely <meshuba.global@gmail.com>
2023-09-05 17:08:58 +0300

    update mainframe

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Contact from "../components/Contact";
 import Main from "../components/Main";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
           content="Ofek Shmuely | Front-End Developer | E-commerce Operator | Content Creator"
         />
         <link rel="icon" href="/fav.png" />
+        <Analytics />
       </Head>
       <Main />
       <About />
